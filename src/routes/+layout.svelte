d3634490971591f376e783f6efa1e79cfe3f5367
Melissa <melissamwaltner@gmail.com>
2023-09-01 12:42:49 -0500

    Remove code that doesn't appear to be doing anything

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,10 @@
 <script>
   import "../app.css";
-  import { page } from '$app/stores';
   import { onMount } from "svelte";
   import Menu from "../lib/components/menu.svelte"
 
   onMount(() => {
-    $page.subscribe((newPage) => {
-      Menu.closeMenu(); // Call a function to close the menu
-    });
+    Menu.closeMenu(); // Call a function to close the menu
   });
 </script>
 
