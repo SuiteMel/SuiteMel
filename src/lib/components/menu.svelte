ae1be0746606ba4add9e4b2f49b2fae0e6b61fec
Melissa <melissamwaltner@gmail.com>
2023-09-05 12:58:44 -0500

    Toggling nav

--- src/lib/components/menu.svelte
+++ src/lib/components/menu.svelte
@@ -1,16 +1,13 @@
 <script>
-  let toggled = false;
+  import { page, navigating } from '$app/stores';
+  let isOpen = false;
 
-  function toggleMenu() {
-    toggled = !toggled;
-  }
-
-  export function closeMenu() {
-    toggled = false;
-  }
+  const toggleMenu = () => {
+    isOpen = !isOpen;
+  };
 </script>
 
-<nav class="absolute min-w-fit w-1/6 -translate-x-full transition duration-300" class:toggled>
+<nav class="absolute min-w-fit w-1/6 -translate-x-full transition duration-300" class:isOpen>
   <button on:click={toggleMenu} class="absolute top-0 left-full border-brand-primary rounded-br border-r-2 border-b-2 p-1 bg-white -ml-0.5 h-10 w-10">
     <img class="" aria-hidden="true" src="/icon-big.png" alt="SuiteMel menu icon">
     <span class="sr-only">Menu</span>
@@ -18,11 +15,11 @@
   <div class="bg-white py-8 px-6 rounded-br border-brand-primary border-r-2 border-b-2">
     <ul class="space-y-3">
       <li>
-        <a class="link-item" href="/">Home</a>
+        <a class="link-item" on:click={toggleMenu} href="/">Home</a>
       </li>
       
       <li>
-        <a class="link-item" href="/frontend-mentor">Frontend Mentor Challenges</a>
+        <a class="link-item" on:click={toggleMenu} href="/frontend-mentor">Frontend Mentor Challenges</a>
       </li>
     </ul>
   </div>
@@ -35,13 +32,16 @@
     text-decoration-thickness: 2px;
     text-decoration-color: theme('colors.brand.secondary');
     font-weight: 500;
-
     &:hover {
       text-decoration-color: transparent;
     }
+
+    &:focus {
+      outline: theme('colors.brand.secondary') solid 1px ;
+    }
   }
 
-  .toggled {
+  .isOpen {
     transform: translateX(0);
   }
 </style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,6 @@
 <script>
   import "../app.css";
-  import { onMount } from "svelte";
   import Menu from "../lib/components/menu.svelte"
-
-  onMount(() => {
-    Menu.closeMenu(); // Call a function to close the menu
-    console.log( 'mounted' );
-  });
 </script>
 
 <Menu />
