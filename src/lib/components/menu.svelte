bd26c55aebeb70962b72560d775a1acff2a1b003
Melissa <melissamwaltner@gmail.com>
2023-09-05 13:45:57 -0500

    Click outside

--- src/lib/components/menu.svelte
+++ src/lib/components/menu.svelte
@@ -5,13 +5,37 @@
   const toggleMenu = () => {
     isOpen = !isOpen;
   };
+
+  const closeMenu = () => {
+    isOpen = false;
+  }
+
+  function clickOutside(node) {
+  
+    const handleClick = event => {
+      if (node && !node.contains(event.target) && !event.defaultPrevented) {
+        node.dispatchEvent(
+          new CustomEvent('click_outside', node)
+        )
+      }
+    }
+
+    document.addEventListener('click', handleClick, true);
+    
+    return {
+      destroy() {
+        document.removeEventListener('click', handleClick, true);
+      }
+    }
+  }
 </script>
 
-<nav class="absolute min-w-fit w-1/6 -translate-x-full transition duration-300" class:isOpen>
+<nav class="absolute min-w-fit w-1/6 -translate-x-full transition duration-300" class:isOpen use:clickOutside on:click_outside={closeMenu}>
   <button on:click={toggleMenu} class="absolute top-0 left-full border-brand-primary rounded-br border-r-2 border-b-2 p-1 bg-white -ml-0.5 h-10 w-10">
     <img class="" aria-hidden="true" src="/icon-big.png" alt="SuiteMel menu icon">
     <span class="sr-only">Menu</span>
   </button>
+  
   <div class="bg-white py-8 px-6 rounded-br border-brand-primary border-r-2 border-b-2">
     <ul class="space-y-3">
       <li>
